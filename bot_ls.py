b52d6e931b2f05bee6caff9e9e35d0a00103bee6
NikitaMikhailov <44605908+NikitaMikhailov@users.noreply.github.com>
2019-07-20 10:49:40 +0300

    Update bot_ls.py

--- bot_ls.py
+++ bot_ls.py
@@ -116,7 +116,7 @@ vk.messages.send(
 def goroscop1():
     spisok_znakov=['aries','taurus','gemini','cancer','leo','virgo','libra','scorpio','sagittarius','capricorn','aquarius','pisces']
     for i in range (0,12):
-        filegor=open('/home/NikMik/bot/goroskop/'+spisok_znakov[i]+'.txt','w')
+        filegor=open(spisok_znakov[i]+'.txt','w')
         filegor.write(((bs4.BeautifulSoup(requests.get("http://astroscope.ru/horoskop/ejednevniy_goroskop/" + spisok_znakov[i] + ".html").text,"html.parser").find('div', 'col-12')).getText().lstrip()))
         filegor.close()
 
@@ -229,7 +229,7 @@ def mainfunc():
 
                     elif event.obj.text in dict8 and flaggoroscop is True:
                         zodiak = dict8[event.obj.text]
-                        f=open('/home/NikMik/bot/goroskop/'+zodiak+'.txt','r')
+                        f=open(zodiak+'.txt','r')
                         goroskp=f.read()
                         f.close()
                         vk.messages.send(  # Отправляем собщение
