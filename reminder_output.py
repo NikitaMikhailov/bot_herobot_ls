26ad802a32219e483cd7c705a3360e6035eedd7e
Mikhailov Nikita <44605908+NikitaMikhailov@users.noreply.github.com>
2019-08-20 18:34:37 +0300

    Update reminder_output.py

--- reminder_output.py
+++ reminder_output.py
@@ -16,12 +16,14 @@ def sent_message(text, user_id):
         random_id=get_random_id(),
         message=text
     )
-
+try:
 #/root/bot_herobot_ls/resurses/
-f=open('/root/bot_herobot_ls/resurses/zametki.txt',encoding='utf8')
-for line in f:
-    zametka=line.split('***#***')
+    f=open('/root/bot_herobot_ls/resurses/zametki.txt',encoding='utf8')
+    for line in f:
+        zametka=line.split('***#***')
     #print(zametka)
-    if line!='\n' and datetime.datetime.now().month==int(zametka[0]) and datetime.datetime.now().day==int(zametka[1]) and datetime.datetime.now().hour==int(zametka[2]) and datetime.datetime.now().minute==int(zametka[3]):
-        sent_message('У меня есть для тебя напоминание:', int(zametka[5]))
-        sent_message(zametka[4].capitalize(),int(zametka[5]))
+        if line!='\n' and datetime.datetime.now().month==int(zametka[0]) and datetime.datetime.now().day==int(zametka[1]) and datetime.datetime.now().hour==int(zametka[2]) and datetime.datetime.now().minute==int(zametka[3]):
+            sent_message('У меня есть для тебя напоминание:', int(zametka[5]))
+            sent_message(zametka[4].capitalize(),int(zametka[5]))
+except Exception as err:
+    sent_message('Возникла ошибка ' + str(err) + ' в напоминаниях',195310233)
