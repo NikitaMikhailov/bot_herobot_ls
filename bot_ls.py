6bd3cf1ea85ca138edb82a409b5b281f39f2940d
NikitaMikhailov <mikhailov-nikita1997@yandex.ru>
2020-02-01 12:12:16 +0300

    Up_02_01_1

--- bot_ls.py
+++ bot_ls.py
@@ -131,8 +131,13 @@ vk.messages.send(
 def goroscop1():
     spisok_znakov=['aries','taurus','gemini','cancer','leo','virgo','libra','scorpio','sagittarius','capricorn','aquarius','pisces']
     for i in range (0,12):
-        filegor=open('/root/bot_herobot_chat/resurses/goroskop_files/'+spisok_znakov[i]+'.txt','w')
-        filegor.write(((bs4.BeautifulSoup(requests.get("http://astroscope.ru/horoskop/ejednevniy_goroskop/" + spisok_znakov[i] + ".html").text,"html.parser").find('div', 'col-12')).getText().lstrip()))
+        f = requests.get(
+            "http://astroscope.ru/horoskop/ejednevniy_goroskop/" + spisok_znakov[i] + ".html")  # .text,"html.parser"
+        f.encoding = 'utf-8'
+        text_gor = (bs4.BeautifulSoup(f.text, "html.parser").find('div', 'col-12'))
+        print(str(str(text_gor).split('\n')[2]).lstrip())
+        filegor = open('resurses/goroskop_files/' + spisok_znakov[i] + '.txt', 'w')  # /root/bot_herobot_chat
+        filegor.write(str(str(text_gor).split('\n')[2]).lstrip())
         filegor.close()
 
 def sentLS(text,user):
